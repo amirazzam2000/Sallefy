d44f803b3af3210422018648aa817e6b87f3f23b
rsmnr <arcadia.youlten@students.salle.url.edu>
2020-03-04 19:50:30 +0100

    SignUpActivityJava

--- app/src/main/java/edu/url/salle/amir/azzam/sallefy/SignUpActivity.java
+++ app/src/main/java/edu/url/salle/amir/azzam/sallefy/SignUpActivity.java
@@ -3,13 +3,40 @@ package edu.url.salle.amir.azzam.sallefy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private Button mRegister;
+    private Button mSignin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mRegister = (Button) findViewById(R.id.register);
+        mSignin = (Button) findViewById(R.id.login2);
+
+        mRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //do nothing
+            }
+        });
+
+        mSignin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //do nothing
+            }
+        });
+
+
+
+
 
 
 
