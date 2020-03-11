231a691ceb1d40759083339a72626fe3eff20657
amirazzam2000 <45884568+amirazzam2000@users.noreply.github.com>
2020-03-11 17:55:36 +0100

    lay out

---
+++ app/src/main/java/edu/url/salle/amir/azzam/sallefy/Model/User.java
@@ -0,0 +1,165 @@
+package edu.url.salle.amir.azzam.sallefy.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class User implements Serializable {
+
+        private int id;
+        private String login;
+        private Object firstName;
+        private Object lastName;
+        private String email;
+        private Object imageUrl;
+        private boolean activated;
+        private String langKey;
+        private String createdBy;
+        private String createdDate;
+        private String lastModifiedBy;
+        private String lastModifiedDate;
+        private int playlists;
+        private int tracks;
+        private int followers;
+        private int following;
+        private List<String> authorities = null;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getLogin() {
+            return login;
+        }
+
+        public void setLogin(String login) {
+            this.login = login;
+        }
+
+        public Object getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(Object firstName) {
+            this.firstName = firstName;
+        }
+
+        public Object getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(Object lastName) {
+            this.lastName = lastName;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public Object getImageUrl() {
+            return imageUrl;
+        }
+
+        public void setImageUrl(Object imageUrl) {
+            this.imageUrl = imageUrl;
+        }
+
+        public boolean isActivated() {
+            return activated;
+        }
+
+        public void setActivated(boolean activated) {
+            this.activated = activated;
+        }
+
+        public String getLangKey() {
+            return langKey;
+        }
+
+        public void setLangKey(String langKey) {
+            this.langKey = langKey;
+        }
+
+        public String getCreatedBy() {
+            return createdBy;
+        }
+
+        public void setCreatedBy(String createdBy) {
+            this.createdBy = createdBy;
+        }
+
+        public String getCreatedDate() {
+            return createdDate;
+        }
+
+        public void setCreatedDate(String createdDate) {
+            this.createdDate = createdDate;
+        }
+
+        public String getLastModifiedBy() {
+            return lastModifiedBy;
+        }
+
+        public void setLastModifiedBy(String lastModifiedBy) {
+            this.lastModifiedBy = lastModifiedBy;
+        }
+
+        public String getLastModifiedDate() {
+            return lastModifiedDate;
+        }
+
+        public void setLastModifiedDate(String lastModifiedDate) {
+            this.lastModifiedDate = lastModifiedDate;
+        }
+
+        public int getPlaylists() {
+            return playlists;
+        }
+
+        public void setPlaylists(int playlists) {
+            this.playlists = playlists;
+        }
+
+        public int getTracks() {
+            return tracks;
+        }
+
+        public void setTracks(int tracks) {
+            this.tracks = tracks;
+        }
+
+        public int getFollowers() {
+            return followers;
+        }
+
+        public void setFollowers(int followers) {
+            this.followers = followers;
+        }
+
+        public int getFollowing() {
+            return following;
+        }
+
+        public void setFollowing(int following) {
+            this.following = following;
+        }
+
+        public List<String> getAuthorities() {
+            return authorities;
+        }
+
+        public void setAuthorities(List<String> authorities) {
+            this.authorities = authorities;
+        }
+
+
+
+
+}
