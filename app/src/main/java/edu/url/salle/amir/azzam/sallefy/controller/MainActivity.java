55dde2d96e3f5124f7cfa3539bc47f727ccfb1e9
amirazzam2000 <45884568+amirazzam2000@users.noreply.github.com>
2020-03-11 17:35:14 +0100

    lay out

--- app/src/main/java/edu/url/salle/amir/azzam/sallefy/controller/MainActivity.java
+++ app/src/main/java/edu/url/salle/amir/azzam/sallefy/controller/MainActivity.java
@@ -5,9 +5,14 @@ import edu.url.salle.amir.azzam.sallefy.R;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextClock;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Objects;
@@ -18,7 +23,10 @@ public class MainActivity extends AppCompatActivity {
     private Button signUp;
     private Button forgotPassword ;
     private int backButtonCount;
-
+    private ImageButton seePassword;
+    private boolean shown;
+    private EditText password;
+    private EditText email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +37,31 @@ public class MainActivity extends AppCompatActivity {
         signIn = (Button) findViewById(R.id.login);
         signUp = (Button) findViewById(R.id.login2);
         forgotPassword = (Button) findViewById(R.id.button_send);
+        seePassword = (ImageButton) findViewById(R.id.imageButton);
+        password = (EditText) findViewById(R.id.password);
+        email = (EditText) findViewById(R.id.username);
+        shown = false ;
 
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 moveToSignUp();
             }
+        });
+
+        seePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!shown) {
+                    password.setTransformationMethod(new PasswordTransformationMethod());
+                    shown = true;
+                }
+                else{
+                    password.setTransformationMethod(null);
+                    shown = false;
+                }
+            }
+
         });
 
         forgotPassword.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +71,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailStr = email.getText().toString().trim();
+                String passwordStr = password.getText().toString();
+
+                    if(emailStr.length()>=1){
+
+                        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+
+                        if (!emailStr.matches(emailPattern))
+                        {
+                            email.setError("Invalid email address");
+                         //   Toast.makeText(getApplicationContext(),"Invalid email address", Toast.LENGTH_SHORT).show();
+                        }
+                        else if (passwordStr.length() < 8){
+                            password.setError("Invalid password");
+                            //Toast.makeText(getApplicationContext(),"Invalid password", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    else{
+                        email.setError("You can't leave the email filed empty!");
+                        //Toast.makeText(getApplicationContext(),"You can't leave the email filed empty!", Toast.LENGTH_SHORT).show();
+                    }
+            }
+        });
 
 
 
--- app/src/main/java/edu/url/salle/amir/azzam/sallefy/controller/SignUpActivity.java
+++ app/src/main/java/edu/url/salle/amir/azzam/sallefy/controller/SignUpActivity.java
@@ -5,9 +5,14 @@ import edu.url.salle.amir.azzam.sallefy.R;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.Objects;
 
@@ -15,6 +20,13 @@ public class SignUpActivity extends AppCompatActivity {
 
     private Button mRegister;
     private Button mSignIn;
+    private EditText username;
+    private EditText first_name;
+    private EditText last_name;
+    private EditText email;
+    private ImageButton seePassword;
+    private boolean shown;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +38,50 @@ public class SignUpActivity extends AppCompatActivity {
 
         mRegister = (Button) findViewById(R.id.register);
         mSignIn = (Button) findViewById(R.id.already_have_account);
+        username = (EditText) findViewById(R.id.username);
+        first_name = (EditText) findViewById(R.id.first_name);
+        last_name = (EditText) findViewById(R.id.last_name);
+        email = (EditText) findViewById(R.id.email);
+        seePassword = (ImageButton) findViewById(R.id.imageButton2);
+         password = (EditText) findViewById(R.id.password);
 
         mRegister.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                //do nothing
+                String usernameStr = username.getText().toString();
+                String firstStr = first_name.getText().toString();
+                String secondStr = last_name.getText().toString();
+                String emailStr = email.getText().toString();
+                String passwordStr = password.getText().toString();
+
+                if(emailStr.length()>=1){
+
+                    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+
+                    if (!emailStr.matches(emailPattern) || emailStr.length() == 0)
+                    {
+                        email.setError("Invalid email address");
+                        //   Toast.makeText(getApplicationContext(),"Invalid email address", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else{
+                    email.setError("Invalid email address");
+                }
+                if (passwordStr.length() < 8){
+                    password.setError("The password must be more than 8 characters");
+                    //Toast.makeText(getApplicationContext(),"Invalid password", Toast.LENGTH_SHORT).show();
+                }
+                if (usernameStr.length() < 1){
+                    username.setError("Invalid username ");
+                }
+                if (firstStr.length() < 1){
+                    first_name.setError("Invalid name");
+                }
+                if(secondStr.length() < 1){
+                    last_name.setError("Invalid name");
+                }
+
             }
         });
 
@@ -39,6 +90,21 @@ public class SignUpActivity extends AppCompatActivity {
             public void onClick(View v){
                 moveToSignIn();
             }
+        });
+
+        seePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!shown) {
+                    password.setTransformationMethod(new PasswordTransformationMethod());
+                    shown = true;
+                }
+                else{
+                    password.setTransformationMethod(null);
+                    shown = false;
+                }
+            }
+
         });
 
 
