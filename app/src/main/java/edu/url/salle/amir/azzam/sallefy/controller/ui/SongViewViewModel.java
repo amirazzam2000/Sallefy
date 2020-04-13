8e177402ceed75155068e7890bbc5e8cb6f66533
kudos01 <fpstoppa@gmail.com>
2020-04-13 12:07:26 +0200

    Song view UI done
    Kudos1

---
+++ app/src/main/java/edu/url/salle/amir/azzam/sallefy/controller/ui/SongViewViewModel.java
@@ -0,0 +1,7 @@
+package edu.url.salle.amir.azzam.sallefy.controller.ui;
+
+import androidx.lifecycle.ViewModel;
+
+public class SongViewViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
